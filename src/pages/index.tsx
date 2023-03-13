5e23852d24a340f8c795908abd87d99ca267e013
eXiga <anton.kostenich@hotmail.com>
2023-03-13 17:59:42 +0100

    remove zustand, will use rq as external store

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
+import { trpc } from "@/utils/trpc";
+import { Link } from "@prisma/client";
+
 export default function Home() {
-  return <div className="h-full rounded-3xl bg-slate-100"></div>;
+  const links = trpc.links.getAll.useQuery(undefined, {
+    refetchOnWindowFocus: false,
+  });
+
+  if (!links.data) {
+    return <div>Loading</div>;
+  }
+
+  return (
+    <div className="h-full rounded-3xl bg-yellow-500">
+      <ul>
+        {links.data.map((link) => {
+          return <li key={link.id}>{link.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
