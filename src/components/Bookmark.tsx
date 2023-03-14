5e36a78deac134f5bbe179e7be3a0c985eb2900c
eXiga <anton.kostenich@hotmail.com>
2023-03-14 18:20:08 +0100

    start working on share button in bookmark actions

--- src/components/Bookmark.tsx
+++ src/components/Bookmark.tsx
@@ -5,24 +5,57 @@ interface BookmarkProps {
   url: string;
 }
 
+interface BookmarkActionButtonProps {
+  imagePath: string;
+  alt: string;
+  onClick: () => void;
+}
+
+function BookmarkActionButton(props: BookmarkActionButtonProps) {
+  return (
+    <a
+      className="rounded-xl cursor-pointer hover:bg-slate-100 active:bg-slate-200"
+      onClick={props.onClick}
+    >
+      <Image src={props.imagePath} height={24} width={24} alt={props.alt} />
+    </a>
+  );
+}
+
 export default function Bookmark(props: BookmarkProps) {
   return (
     <div className="flex flex-row items-center justify-between pt-10 pr-10">
       <div className="flex flex-col items-start justify-center pl-10">
         <a
           href={props.url}
-          className="text-black text-lg truncate tracking-wide"
+          target="_blank"
+          rel="noreferrer"
+          className="text-black text-lg truncate tracking-wide hover:underline"
         >
           {props.title}
         </a>
         <p className="text-slate-400 text-xs truncate tracking-normal">
-          {props.url}
+          {props.url.replace("https://", "")}
         </p>
       </div>
       <div className="flex flex-row gap-10">
-        <Image src="images/share.svg" height={24} width={24} alt="Share" />
-        <Image src="images/delete.svg" height={24} width={24} alt="Delete" />
-        <Image src="images/dots.svg" height={24} width={24} alt="Menu" />
+        <BookmarkActionButton
+          imagePath="images/share.svg"
+          alt="Share"
+          onClick={async () => {
+            await navigator.clipboard.writeText(props.url);
+          }}
+        />
+        <BookmarkActionButton
+          imagePath="images/delete.svg"
+          alt="Delete"
+          onClick={() => {}}
+        />
+        <BookmarkActionButton
+          imagePath="images/dots.svg"
+          alt="Menu"
+          onClick={() => {}}
+        />
       </div>
     </div>
   );
