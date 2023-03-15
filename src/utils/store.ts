a5a7490e344a7fc52501cdb29a101424867f2340
eXiga <anton.kostenich@hotmail.com>
2023-03-15 15:40:14 +0100

    export SelectedMode type from store

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 
 const SelectedModes = ["All", "Must", "Consider", "Later"] as const;
-type SelectedMode = typeof SelectedModes[number];
+export type SelectedMode = typeof SelectedModes[number];
 
 interface State {
   isToastVisible: boolean;
